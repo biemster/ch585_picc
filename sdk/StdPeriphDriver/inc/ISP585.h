49e6a46680f13da04e30e7b773c4ec74ff7fb5cc
Lars Beemster <biemster@users.noreply.github.com>
2025-10-01 16:29:03 +0200

    update to sdk v1.6

--- sdk/StdPeriphDriver/inc/ISP585.h
+++ sdk/StdPeriphDriver/inc/ISP585.h
@@ -1,8 +1,8 @@
 /* CH585 Flash-ROM & Data-Flash  */
 /* Website:  http://wch.cn       */
 /* Email:    tech@wch.cn         */
-/* Author:   W.ch 2020.06        */
-/* V1.0 FlashROM library for USER/BOOT */
+/* Author:   W.ch 2025.05        */
+/* V1.1 FlashROM library for USER/BOOT */
 /* for the target in USER code area on the chip divided into USER code area and BOOT area */
 /* 用于具有用户代码区和引导区的芯片、操作目标为用户代码区的情况，
    可以在用户代码中被调用（IAP，擦写自身），也可以在引导代码中被调用（更新用户代码） */
@@ -19,6 +19,8 @@
      256 bytes/page for writing, EEPROM_WRITE support one byte or more byte writing, but multiple of 256 is the best,
      0.25KB/4KB (256/4096 bytes) bytes/block for erasing, so multiple of 256 or 4096 is the best */
 
+#ifndef __ISP58x_H__
+#define __ISP58x_H__
 
 #ifndef EEPROM_PAGE_SIZE
 #define EEPROM_PAGE_SIZE    256                       // Flash-ROM & Data-Flash page size for writing
@@ -27,6 +29,7 @@
 //#define EEPROM_MIN_ER_SIZE  EEPROM_BLOCK_SIZE         // Flash-ROM  minimal size for erasing
 #define EEPROM_MIN_WR_SIZE  1                         // Data-Flash minimal size for writing
 #define EEPROM_MAX_SIZE     0x8000                    // Data-Flash maximum size, 32KB
+//#define EEPROM_MAX_SIZE     0x80000                   // Data-Flash maximum size for 584X, 512KB
 #endif
 #ifndef FLASH_MIN_WR_SIZE
 #define FLASH_MIN_WR_SIZE   4                         // Flash-ROM minimal size for writing
@@ -53,6 +56,8 @@
 #endif
 
 #define ROM_CFG_MAC_ADDR	0x7F018			// address for MAC address information
+#define ROM_CFG_CHIP_ID     0x7F024
+#define DEF_CHIP_ID_CH584X  0x08
 #define ROM_CFG_BOOT_INFO	0x7DFF8			// address for BOOT information
 
 /**
@@ -60,8 +65,8 @@
  *
  * @param   cmd         - CMD_* for caller from FlashROM or RAM.
  * @param   StartAddr   - Address of the data to be process.
-
- * @param   Buffer      - Pointer to the buffer where data should be process, Must in RAM and be aligned to 4 bytes. * @param   Length      - Size of data to be process, in bytes.
+ * @param   Buffer      - Pointer to the buffer where data should be process, Must in RAM and be aligned to 4 bytes.
+ * @param   Length      - Size of data to be process, in bytes.
  *
  * @return  0-SUCCESS  (!0)-FAILURE
  */
@@ -131,7 +136,17 @@ extern uint32_t FLASH_EEPROM_CMD( uint8_t cmd, uint32_t StartAddr, void *Buffer,
  *
  * @return  0-SUCCESS  (!0)-FAILURE
  */
-#define EEPROM_ERASE(StartAddr,Length)              FLASH_EEPROM_CMD( CMD_EEPROM_ERASE, StartAddr, NULL, Length )
+__attribute__((always_inline)) RV_STATIC_INLINE uint32_t EEPROM_ERASE(uint32_t StartAddr, uint32_t Length)
+{
+    if((*((uint32_t*)0x7F024)&0x0F) == 0x08 )
+    {
+        if(Length%EEPROM_BLOCK_SIZE)
+        {
+            while(1);
+        }
+    }
+    return FLASH_EEPROM_CMD( CMD_EEPROM_ERASE, StartAddr, NULL, Length );
+}
 
 /**
  * @brief   write Data-Flash data block
@@ -176,3 +191,4 @@ extern uint32_t FLASH_EEPROM_CMD( uint8_t cmd, uint32_t StartAddr, void *Buffer,
  */
 #define FLASH_ROM_VERIFY(StartAddr,Buffer,Length)   FLASH_EEPROM_CMD( CMD_FLASH_ROM_VERIFY, StartAddr, Buffer, Length )
 
+#endif
